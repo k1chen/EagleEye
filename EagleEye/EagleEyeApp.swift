2c1b6c61798e8b772deff814cc71ff6f2034ee76
k1chen <chenaun@bc.edu>
2025-04-24 20:48:23 -0400

    Initial Commit

---
+++ EagleEye/EagleEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleEyeApp.swift
+//  EagleEye
+//
+//  Created by Kenneth Chen on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EagleEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
